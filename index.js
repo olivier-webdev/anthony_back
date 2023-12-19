80305bb194180a75cbe877d5af798befa7c02c89
olivier-webdev <olivier.webdev@gmail.com>
2023-12-19 11:23:41 +0100

    modify root index.js

--- index.js
+++ index.js
@@ -10,24 +10,27 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static(path.join(__dirname, "uploads")));
 
-const port = process.env.MYSQL_PORT || 8000;
+const port = process.env.PORT || 8000;
 require("./database/index");
 const routes = require("./routes");
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://les-des-gommes.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE,PATCH");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://les-des-gommes.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE,PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
 });
 app.use(routes);
 app.use("*", (req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 app.get("/", (_, res) => {
-    res.send(JSON.stringify("API WORKING"));
+  res.send(JSON.stringify("API WORKING"));
 });
-app.listen(port, () => {
-    console.log(`serveur Node écoutant sur le port ${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`serveur Node écoutant sur le port ${port}`);
 });
